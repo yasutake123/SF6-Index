9173c028bd8c2d6078e9c0bae32ded1d4c73bd01
rin_desktop <lintailanganwu58@gmail.com>
2026-04-26 15:57:31 +0900

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/github/yasutake123/sf6_index/Sf6IndexApplication.java
@@ -0,0 +1,13 @@
+package com.github.yasutake123.sf6_index;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sf6IndexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sf6IndexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/yasutake123/sf6_index/Sf6IndexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.yasutake123.sf6_index;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sf6IndexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
