da084f6e1cd9515d5ffabd7a8864697fb14acc25
rin_desktop <lintailanganwu58@gmail.com>
2026-05-05 23:20:17 +0900

    feat: update dto for v1.2 format

--- src/main/java/com/github/yasutake123/sf6_index/dto/MotionFrame.java
+++ src/main/java/com/github/yasutake123/sf6_index/dto/MotionFrame.java
@@ -1,9 +1,17 @@
 package com.github.yasutake123.sf6_index.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class MotionFrame {
     private String startup;
     private String active;
     private String total;
+    @JsonProperty("startup_val")
+    private Integer startupVal;
+    @JsonProperty("active_val")
+    private Integer activeVal;
+    @JsonProperty("total_val")
+    private Integer totalVal;
 
     public String getStartup() { return startup; }
     public void setStartup(String startup) { this.startup = startup; }
@@ -13,4 +21,13 @@ public class MotionFrame {
     
     public String getTotal() { return total; }
     public void setTotal(String total) { this.total = total; }
+
+    public Integer getStartupVal() { return startupVal; }
+    public void setStartupVal(Integer startupVal) { this.startupVal = startupVal; }
+    
+    public Integer getActiveVal() { return activeVal; }
+    public void setActiveVal(Integer activeVal) { this.activeVal = activeVal; }
+    
+    public Integer getTotalVal() { return totalVal; }
+    public void setTotalVal(Integer totalVal) { this.totalVal = totalVal; }
 }
